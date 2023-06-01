8754d671b06ee5e7c2c5f97acb400a552cee48f5
SMyumber <ycm110827@gmail.com>
2023-06-01 14:13:56 +0900

    init

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/login'
+  get 'sessions/welcome'
+  get 'users/new'
+  get 'users/create'
+  root "articles#index"
+
+  resources :articles do
+    resources :comments
+  end
+end
